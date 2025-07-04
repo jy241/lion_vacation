0e4f5e9851c1f7435c3575f0abadc421f8ba6c58
jy241 <155972130+jy241@users.noreply.github.com>
2025-07-04 15:00:12 +0900

    Initial commit

---
+++ src/compontents/Header.jsx
@@ -0,0 +1,4 @@
+function Header() {
+  return <header>수강신청 시스템</header>;
+}
+export default Header;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,4 @@
+function HomePage() {
+  return <div>홈페이지입니다.</div>;
+}
+export default HomePage;
