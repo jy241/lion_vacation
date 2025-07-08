bcf371efc7fbcb1f20601ed399e6e0ce1664aebb
jy241 <155972130+jy241@users.noreply.github.com>
2025-07-08 19:26:13 +0900

    rafator: color change

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -29,7 +29,7 @@ export default function LoginPage() {
 
   return (
     <div className="login-page">
-      <h2>로그인</h2>
+      <h2 className='login-header'>로그인</h2>
       <input
         type="email"
         placeholder="이메일"
